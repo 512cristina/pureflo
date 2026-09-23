f0fd61b9f812c6079fbb1e83dff58af58336860e
512cristina <donosoc1@gmail.com>
2026-09-23 15:15:31 -0500

    meta tag for algolia search validation to test

--- wp-content/themes/pureflo/header.php
+++ wp-content/themes/pureflo/header.php
@@ -4,6 +4,7 @@
 <title>PureFlo | <?php wp_title(); ?></title>
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="algolia-site-verification"  content="46223365ED2E282E" />
 
 <?php wp_head(); ?>
 </head>
