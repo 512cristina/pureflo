180c0d22ddbc85e0d171de142580ccc11dd9136f
512cristina <donosoc1@gmail.com>
2026-09-23 17:08:42 -0500

    algolia script to test out.

--- wp-content/themes/pureflo/footer.php
+++ wp-content/themes/pureflo/footer.php
@@ -2,5 +2,15 @@
 <?php get_template_part('template-parts/site-footer'); ?>
 
 <?php wp_footer(); ?>
+
+
+<!--
+  Ensure these containers exist on your page:
+  - #algoliaSearch
+-->
+
+<!-- Load the Algolia Experiences library at the end of <body> -->
+<script src="https://cdn.jsdelivr.net/npm/@algolia/experiences/dist/experiences.js?appId=R15X7CYQGV&apiKey=43f758f450d238c12874639323175f41&experienceId=R15X7CYQGV&env=prod"></script>
+
 </body>
 </html>
